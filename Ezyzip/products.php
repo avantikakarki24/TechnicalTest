8affeb17e039898b6bd72feddc28ed2f1fd12e7d
avantikakarki24 <37787397+avantikakarki24@users.noreply.github.com>
2021-05-13 08:20:17 +1200

    Add files via upload

---
+++ Ezyzip/products.php
@@ -0,0 +1,16 @@
+<?php 
+$products = [
+    [ "name" => "Sledgehammer", "price" => 125.75],
+    [ "name" => "Axe", "price" => 190.50],
+    [ "name" => "Bandsaw", "price" => 562.131],
+    [ "name" => "Chisel", "price" => 12.9],
+    [ "name" => "Hacksaw", "price" => 18.45],
+];
+
+$product_count = [
+    "Sledgehammer" => 0,
+    "Axe" => 0,
+    "Bandsaw" => 0,
+    "Chisel" => 0,
+    "Hacksaw" => 0,
+];
