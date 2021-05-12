999dcdc3cf803b9a171d9c8adccb199d0fc36228
avantikakarki24 <37787397+avantikakarki24@users.noreply.github.com>
2021-05-13 08:36:29 +1200

    Add files via upload

---
+++ ShoppingCart/header.php
@@ -0,0 +1,44 @@
+<?php
+
+include('mycart.php');
+$cart = array();
+if($_SESSION['cart'] != null){
+  $cart = $_SESSION['cart'];
+}
+if($_SESSION['product_count'] != null){
+  $quantity = $_SESSION['product_count'];
+}
+
+?>
+<!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+        </li>
+
+      </ul >
+      <div>
+        <a href="./manage_cart.php" class = "btn btn-outline-success"> My Cart <?php echo count($cart) ?> </a>
+      </div>
+    </div>
+  </div>
+</nav>
+      
+</body>
+</html>
